7b9d04c1dda0ce0fbb157801eabe0424ac9987ca
foadyousef <fyousef@mtu.edu>
2019-06-19 13:52:50 -0400

    fixing issues

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 colors = {
     'text': '#7FDBFF'
 }

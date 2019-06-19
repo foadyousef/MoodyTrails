3919a8385d7f395f1d400cc6469dd1dd62b88d95
foadyousef <fyousef@mtu.edu>
2019-06-19 11:46:01 -0400

    initial app deploy

---
+++ app.py
@@ -0,0 +1,66 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import trail_stat as ts
+from datetime import datetime as dt
+from datetime import date, timedelta
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+colors = {
+    'text': '#7FDBFF'
+}
+
+markdown_text = "**A web application to make Real-time prediction of Trail Conditions.**"
+
+app.layout = html.Div(children=[
+    html.H1(children='MoodyTrails', style={'textAlign': 'center',
+        'color': colors['text']}),
+
+    dcc.Markdown(children=markdown_text),
+
+
+    html.Div(children='''
+        Step 1: Pick a Date
+    ''', style={'textAlign': 'left','fontSize': 14}),
+
+
+    dcc.DatePickerSingle(id='date-picker-single', date= dt.today().date()),
+
+    html.Div(children='''
+        Step 2. Pick a trail
+    ''', style={'textAlign': 'left','fontSize': 14}),
+
+
+    dcc.Dropdown(id='my-id',
+    options=[
+        {'label': 'Adams', 'value': 'ADM'},
+        {'label': 'Washington', 'value': 'WSH'}
+    ],
+    value='ADM'),
+
+    html.Div(id='my-div')
+
+    #dcc.Input(id='my-id', value='initial value', type='text'),
+    #html.Div(id='my-div')
+
+
+
+
+
+])
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value'),
+    Input(component_id='date-picker-single', component_property='date')])
+
+def update_output_div(trailname, userdate):
+    s = ts.trail_stat(trailname, userdate)
+    return s
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ trail_stat.py
@@ -0,0 +1,77 @@
+import requests
+import numpy as np
+import pandas as pd
+import os
+import sys
+from datetime import datetime as dt
+from datetime import date, timedelta
+import NOAA_Historical_weather as noaa
+from sklearn.externals import joblib
+
+
+def trail_stat(trailname, userdate):
+    
+    
+    # NOAA model parameters
+    mytoken = 'JNYovzhikMxTKdSuBEYotIIoYaHzJPLd'
+    stationid = 'GHCND:USC00270690'
+    datasetid = 'GHCND'
+    
+    # Julain Date
+    d = dt.today().date()
+    jday = d.timetuple()
+    jday = jday.tm_yday
+    
+    # Date for NOAA
+    print('##################################')
+    print(userdate)
+    date = dt.strptime(userdate, '%Y-%m-%d')
+    date = date - timedelta(days=3)
+    date = str(date.date())
+    #print(date)
+
+    
+    # Make a call to NOAA
+    wd = noaa.get_weather(stationid, datasetid, date, date, mytoken)
+    
+    
+    # Make a dataframe from observations:
+    try: 
+        PREC = 0
+        TMAX = 0
+        TMIN = 0
+        SNOW = 0
+        PREC = wd['value'][wd['datatype']=='PRCP'].values[0]
+        SNOW = wd['value'][wd['datatype']=='SNOW'].values[0]
+        TMAX = wd['value'][wd['datatype']=='TMAX'].values[0]
+        TMIN = wd['value'][wd['datatype']=='TMIN'].values[0]
+        
+        # Populate the Numbers (check this one)
+        data = [jday,PREC,TMAX,TMIN,SNOW]
+        X = np.asarray(data)
+        # Reshape the data
+        X = X.reshape(1, -1)
+        # Load the RF model:
+        ADM = joblib.load('adams_RF.sav')
+        WSH = joblib.load('adams_RF.sav')
+        # Feed the numbers into the model
+        
+        if trailname =='ADM':
+            print('fitting the model for ADM')
+            p = ADM.predict(X)
+        if trailname == 'WSH':
+            print('fitting the model for WSH')
+            p = WSH.predict(X)
+        # Publish the model prediction
+        print(p[0])
+        if p[0] == 0:
+            stat = 'Trail is not in ideal condition!'
+            return stat
+        else:
+            stat = 'Trail condition is ideal for hiking!'
+            return stat
+    
+    except:
+        print('Weather data might not be available!')
+    
+    

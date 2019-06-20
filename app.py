5a9d2f7a77d9b4a9078a34b5adfd9cb19d1d8cdb
foadyousef <fyousef@mtu.edu>
2019-06-19 21:14:22 -0400

    adding the icon

--- app.py
+++ app.py
@@ -5,55 +5,57 @@ import trail_stat as ts
 from datetime import datetime as dt
 from datetime import date, timedelta
 from dash.dependencies import Input, Output
+import base64
 
+# Layout CSS
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# Style CSS or bootstrap
+# The image
+image_filename = 'trail1.jpg'
+encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+#Without this line, the app does not work on Heroku
 server = app.server
 
-colors = {
-    'text': '#7FDBFF'
-}
+colors = {'text': '#7FDBFF'}
 
 markdown_text = "**A web application to make Real-time prediction of Trail Conditions.**"
 
-app.layout = html.Div(children=[
-    html.H1(children='MoodyTrails', style={'textAlign': 'center',
-        'color': colors['text']}),
 
-    dcc.Markdown(children=markdown_text),
+app.layout = html.Div(
 
+	html.Div(children=[
+		html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()), style={'height' : '50%','width' : '50%'}),
+		dcc.Markdown(children=markdown_text),
+		html.Div(children='''
+		Step 1: Pick a Date
+		''', style={'textAlign': 'left','fontSize': 14}),
+		dcc.DatePickerSingle(id='date-picker-single', date= dt.today().date()),
+		html.Div(children='''
+			Step 2. Pick a trail
+			''', style={'textAlign': 'left','fontSize': 14}),
+		dcc.Dropdown(id='my-id',
+			options=[
+			{'label': 'Adams', 'value': 'ADM'},
+			{'label': 'Washington', 'value': 'WSH'}
+			],
+			value='ADM'),
+		html.Div(id='my-div')
+		#dcc.Input(id='my-id', value='initial value', type='text'),
+		#html.Div(id='my-div')
+		], style={'columnCount': 2})
 
-    html.Div(children='''
-        Step 1: Pick a Date
-    ''', style={'textAlign': 'left','fontSize': 14}),
-
-
-    dcc.DatePickerSingle(id='date-picker-single', date= dt.today().date()),
-
-    html.Div(children='''
-        Step 2. Pick a trail
-    ''', style={'textAlign': 'left','fontSize': 14}),
-
-
-    dcc.Dropdown(id='my-id',
-    options=[
-        {'label': 'Adams', 'value': 'ADM'},
-        {'label': 'Washington', 'value': 'WSH'}
-    ],
-    value='ADM'),
-
-    html.Div(id='my-div')
-
-    #dcc.Input(id='my-id', value='initial value', type='text'),
-    #html.Div(id='my-div')
+	)
 
 
 
 
 
-])
+
+
 
 @app.callback(
     Output(component_id='my-div', component_property='children'),
